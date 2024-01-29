b3b6fdd504aa44a5d1d5242825f1341efe7613df
Pavlo1508 <pasha150898@gmail.com>
2024-01-29 23:09:40 +0100

    hw 04

--- js/task-2.js
+++ js/task-2.js
@@ -1,13 +1,21 @@
-function makeArray(firstArray, secondArray, maxLength) {
-	const newArr = firstArray.concat(secondArray);
-	if (newArr.length >= maxLength) {
-		return newArr.slice(0, maxLength)
+function calcAverageCalories(days) {
+	let averageCalories;
+	let totalCalories = 0;
+	for (let x of days) {
+		totalCalories  += x.calories;
 	}
+	averageCalories = totalCalories / Object.keys(days).length;
+	return averageCalories
 }
 
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+);
